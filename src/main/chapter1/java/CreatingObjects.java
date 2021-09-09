01b833dc129b8f9c6f7023167ee68995486c7b54
Relativiteit <alejocain@gmail.com>
2021-09-09 06:02:18 +0200

    Constructors

--- src/main/chapter1/java/CreatingObjects.java
+++ src/main/chapter1/java/CreatingObjects.java
@@ -1,2 +1,19 @@
 public class CreatingObjects {
+    private String name = "Fried rice";
+    {System.out.println("Making a new dish");}
+
+    // constructor
+    public CreatingObjects(){
+        name = "Chahan";
+        System.out.println("Setting Constructor");}
+
+    // main function to run it all
+    public static void main(String[] args) {
+
+        // type, variable name, new constructor
+        CreatingObjects aObject = new CreatingObjects();
+        System.out.println(aObject.name);
+
+    }
+
 }
