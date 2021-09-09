876f6019435cec62c33d4efae6b1904bc20e6cd2
Relativiteit <alejocain@gmail.com>
2021-09-09 05:33:42 +0200

    Basic class, getter and setter

--- src/main/chapter1/java/UnderstandingJavaClassStructure.java
+++ src/main/chapter1/java/UnderstandingJavaClassStructure.java
@@ -1,3 +1,11 @@
+// base class
 public class UnderstandingJavaClassStructure {
-
+    String name;
+    // gette
+    public String getName(){
+    return name;}
+    // setter
+    public void setName(String newName){
+        name = newName;
+    }
 }
