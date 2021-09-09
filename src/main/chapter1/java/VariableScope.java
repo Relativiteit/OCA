92511a145b85ac2148a439e5cfb4a25f719bdf97
Relativiteit <alejocain@gmail.com>
2021-09-09 15:06:35 +0200

    Variable scope aka where does a code block begin or close

--- src/main/chapter1/java/VariableScope.java
+++ src/main/chapter1/java/VariableScope.java
@@ -1,2 +1,24 @@
+/**
+ * @author Alejo Cain
+ * @date 09-11-2021
+ */
 public class VariableScope {
+
+    public void eatMore(boolean hungry, int amountOfFood){
+        int roomInBelly = 5;
+        if(hungry){
+            boolean timeToEat = true;
+            while (amountOfFood > 0){
+                int amountEaten =2;
+                roomInBelly = roomInBelly - amountEaten;
+                amountOfFood = amountOfFood - amountEaten;
+            }
+        }
+
+    }
+    public static void main(String[] args)
+{
+    System.out.println();
+        }
+
 }
