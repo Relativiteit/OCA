668915019a7d73dea6f06024369795340370fab9
Relativiteit <alejocain@gmail.com>
2021-09-09 14:22:45 +0200

    Primitives and default values

--- src/main/chapter1/java/ObjectReferencesPrimitives.java
+++ src/main/chapter1/java/ObjectReferencesPrimitives.java
@@ -1,2 +1,38 @@
 public class ObjectReferencesPrimitives {
+
+    static String name = " test";
+    boolean questionLife;
+    private int numObjects = 5;
+    double numberDouble = 2.0;
+    float numberFloat = 3.0f;
+    long numberLong = 4043;
+    short numberShort = 2;
+    /**
+     * Primitives   -   Default
+     * Boolean      -   False
+     * Int          -   0
+     * double       -   0.0
+     * float        -   0F
+     * long         -   0L  small l works too but use big L for readability
+     * short        -   0
+     * string       -   null
+     */
+
+   public ObjectReferencesPrimitives() {
+       name = "Japanese";
+       numObjects = 20;
+       questionLife = true;
+       numberDouble = 3.0;
+       numberFloat = 5.0f;
+       numberLong = 60000;
+       numberShort = 1;
+    }
+    public static void main(String[] args)
+    {
+        ObjectReferencesPrimitives garden = new ObjectReferencesPrimitives();
+        System.out.println(garden.name);
+        System.out.println(garden.numberFloat);
+        System.out.println(garden.numObjects);
+    }
+
 }
