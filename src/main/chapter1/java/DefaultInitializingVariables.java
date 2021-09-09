4c8e081778d655a5a6144e380a1159e90898b9bf
Relativiteit <alejocain@gmail.com>
2021-09-09 14:53:41 +0200

    Default values

--- src/main/chapter1/java/DefaultInitializingVariables.java
+++ src/main/chapter1/java/DefaultInitializingVariables.java
@@ -1,2 +1,16 @@
-public class DefaultInitializingVariables {
+public class DefaultInitializingVariables
+
+    /**
+     * Variable types   -   Default initialization
+     * boolean false
+     * byte, short, int, long           0
+     * float, double                    0.0
+     * char                             '\u0000
+     * All object references (everything else) null
+     */
+    {
+        public static void main(String[] args)
+        {
+        System.out.println("Hello default values");
+    };
 }
