2fe2ad8297893c6648980253a0c0ec2482451cc6
Relativiteit <alejocain@gmail.com>
2021-09-14 10:48:20 +0200

    UnaryOperators main code

--- src/main/chapter2/UnaryOperators.java
+++ src/main/chapter2/UnaryOperators.java
@@ -5,4 +5,25 @@ public class UnaryOperators {
      * ++ Increments a value by 1
      * --
      */
-}
+
+    public static void main(String[] args) {
+        double jobs, applications;
+        int counter;
+        double succesrate = 0.2;
+
+        counter = 0;
+
+        for(jobs = 1; jobs <= 400; jobs++){
+            applications = jobs * succesrate;
+            System.out.println("The succesfull applications is aprox this rate" + applications + "from about this many jobs " + jobs);
+
+            counter ++;
+            // every 5 lines print a empty line
+            if(counter == 5)
+            {
+                System.out.println();
+                counter = 0; // reset the counter
+    }
+
+    }
+}}
