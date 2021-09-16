833884631dfa8c3e551500d71e598a61fe820e37
Relativiteit <alejocain@gmail.com>
2021-09-16 18:11:08 +0200

    Equality code

--- src/main/chapter3/Equality.java
+++ src/main/chapter3/Equality.java
@@ -1,2 +1,29 @@
+/**
+ * @author Alejo Cain
+ * @date 16-09-2021
+ */
 public class Equality {
+
+    public static void main(String[] args) {
+
+
+        // equal 2 ways == and .equals()
+
+        Tiger t1 = new Tiger();
+        Tiger t2 = new Tiger();
+        Tiger t3 = t1;
+        System.out.println(t1 == t3); // should result in true
+        System.out.println(t1 == t2); // not pointing at the same object so false
+        System.out.println(t1.equals(t3)); // true
+        System.out.println(t3.equals(t2)); // false
+        System.out.println(t1.age + t1.name);
+
+    }
+
+    static class Tiger{
+        String name = "Tigger";
+        int age = 1000;
+
+    }
 }
+
--- src/main/chapter3/UsingStringbuilder.java
+++ src/main/chapter3/UsingStringbuilder.java
@@ -1,4 +1,26 @@
+/**
+ * @author Alejo Cain
+ * @date 16-09-2021
+ */
+
 public class UsingStringbuilder {
+    public static void main(String[] args)
+    {
+        // creation, creation with args, creation with varargs
+        StringBuilder item1 = new StringBuilder();
+        // contains string dobotsu
+        StringBuilder item2 = new StringBuilder("dobotsu");
+        // has capacity of 10 chars
+        StringBuilder item3 = new StringBuilder(10);
+        System.out.println(item3.length()); // 0
+        // append()
+        item3.append("milk");
+        System.out.println(item3.length()); // 4
+        // indexof()
+        System.out.println(item3.indexOf("k")); // get index of the letter k
+        // System.out.println(item1.insert(0, "oogaaa"));; // not possible capacity is 0
+        System.out.println(item2.insert(0, "floopy"));
 
-
+        System.out.println(item2);
+    }
 }
