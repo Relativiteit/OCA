ceea9310be9aa991de0fbee1274e29045cc9f949
Relativiteit <alejocain@gmail.com>
2021-09-21 23:08:31 +0200

    Arrays, for loops, nested for loops

--- src/main/chapter3/javaArrays.java
+++ src/main/chapter3/javaArrays.java
@@ -1,2 +1,18 @@
-public class javaArrays { // empty the shame !!!!
+/**
+ * @author Alejo Cain
+ * @date 21-09-2021 Time: 23:07
+ */
+
+public class javaArrays {
+    public static void main(String[] args)
+    {
+        int[][] numbers = { {1,2,3,4}, {5,6,7}};
+        // for loop
+        for(int i = 0; i < numbers.length; ++i){
+            // nested for loop
+            for(int j = 0; j < numbers[i].length; ++j){
+                System.out.println(numbers[i][j]);
+            }
+        }
+    }
 }
