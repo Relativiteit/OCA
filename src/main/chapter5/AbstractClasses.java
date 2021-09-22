f02bb0734cbf2df72387b1d926f964e82f518026
Relativiteit <alejocain@gmail.com>
2021-09-22 16:25:51 +0200

    AbstractClasses

--- src/main/chapter5/AbstractClasses.java
+++ src/main/chapter5/AbstractClasses.java
@@ -1,2 +1,36 @@
-public class AbstractClasses {
+/**
+ * @author Alejo Cain
+ * @Date 22-09-2021 time: 16:02
+ */
+// abstract class is used as the template for subclasses
+
+abstract class Architect {
+    public abstract String getName();
 }
+abstract class ExtendAbstract extends Architect{
+    public abstract void code();
+}
+public class AbstractClasses extends ExtendAbstract {
+    public String getName() {
+        return "LeetCode";
+    }
+    public void code(){
+        System.out.println("The coder is really load on her mechanical keyboard");
+    }
+
+    public static void main(String[] args) {
+        Architect Jerome = new Architect() {
+            @Override
+            public String getName() {
+                return null;
+            }
+        };
+            Architect jeromeDeciple = Jerome;
+        System.out.println(jeromeDeciple.getName());
+
+        }
+    }
+
+
+
+
--- src/main/chapter5/ClassInheritance.java
+++ src/main/chapter5/ClassInheritance.java
@@ -1,2 +1,64 @@
+/**
+ * @author Alejo Cain
+ * @Date 22-09-2021 time 12:16
+ */
+
+ class Roadbike {
+        // only 1 level inheritance
+        public int gear;
+        public int speed;
+
+        public Roadbike(int gear, int speed){
+            this.gear = gear;
+            this.speed = speed;
+        }
+        // RoadBike has 3 methods
+        public void applyBrake(int decrement)
+        {
+            speed -= decrement;
+        }
+
+        public void speedUp(int increment)
+        {
+            speed += increment;
+        }
+        public String toString(){
+            return("Number of gear are" + gear + "\n" + "speed of bicycle is " + speed);
+        }
+    }
+
+class GravelBike extends Roadbike {
+
+    // the GravelBIke subclass adds one more field
+    public int seatHeight;
+
+    // the GRavelBike subclass has one constructor
+    public GravelBike(int gear, int speed, int startHeight)
+    {
+        // invoking base-class(Bicycle) constructor
+        super(gear, speed);
+        seatHeight = startHeight;
+    }
+
+    // the GRavelBike subclass adds one more method
+    public void setSeatHeight(int newValue)
+    {
+        seatHeight = newValue;
+    }
+
+    // Overriding toString() method of ClassInheritance for more info.
+    @Override public String toString()
+    {
+        return (super.toString() + "\nseat height is " + seatHeight);
+    }
+}
+
+// public class with driver(main) class
 public class ClassInheritance {
+     public static void main(String args[])
+     {
+         GravelBike CanyonGrail = new GravelBike(22, 40, 30);
+         System.out.println(CanyonGrail.toString());
+
+     }
 }
