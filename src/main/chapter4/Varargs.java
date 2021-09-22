11d3bcc28531f0dc33f6adc81190f003071a0afa
Relativiteit <alejocain@gmail.com>
2021-09-22 02:03:06 +0200

    varargs

--- src/main/chapter4/Varargs.java
+++ src/main/chapter4/Varargs.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * @author Alejo Cain
+ * @Date 22-09-2021 time: 01:40
+ */
+// use varargs when the method input is variable
+
 public class Varargs {
+
+    String small = "Chibi";
+
+    public String hungryMethod(String ...result) {
+    Scanner tempo = new Scanner(System.in);
+     result += tempo
+        System.out.println(result.length());
+        return result;
+
+
+    }
+
+
+    public static void main(String[] args) {
+    hungryMethod();
+        System.out.println("weird stuff ");
+
+    }
 }
