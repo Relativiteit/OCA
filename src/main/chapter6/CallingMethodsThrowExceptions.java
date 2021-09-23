0756e5bf4bd2fd647ee49c299dc75851afa10671
Relativiteit <alejocain@gmail.com>
2021-09-23 23:58:55 +0200

    Calling methods THrow Exceptions

--- src/main/chapter6/CallingMethodsThrowExceptions.java
+++ src/main/chapter6/CallingMethodsThrowExceptions.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class CallingMethodsThrowExceptions {
+
+
+    static void inPutProblem() {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println(" ");
+        } catch (Exception e) {
+            System.out.println("Please give a input: ");
+        }
+    }
+
+    public static void main(String[] args) {
+        inPutProblem();
+
+    }
+
 }
