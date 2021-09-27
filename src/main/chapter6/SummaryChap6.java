884bcd0f62216b636d1e3f593397f3224417c6c7
Relativiteit <alejocain@gmail.com>
2021-09-27 02:54:07 +0200

    Summarychap6 simple

--- src/main/chapter6/SummaryChap6.java
+++ src/main/chapter6/SummaryChap6.java
@@ -1,5 +1,24 @@
 public class SummaryChap6 {
 
-    // combination of things learned from oCA book of exceptions
+    // combination of things learned from chapter 6
+    public static void makeTrouble(){
+        try{
+            int[] goat = {1,2,34,5};
+        System.out.println(goat[6]);}
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Check your index");
+        }
+    }
 
+    public static void main(String[] args) {
+        int age = 5;
+        int goat = 100;
+        String programmer = "Pikachu";
+        for(int i = 0; age < goat; i++){
+            age += 1;
+            makeTrouble(); // called method
+        }
+
+
+    }
 }
